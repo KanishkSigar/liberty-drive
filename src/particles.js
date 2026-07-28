f4d09c5ea295cf80c59309e8e5da329ed4728e89
KanishkSigar <kanishksigar@gmail.com>
2026-07-28 10:00:01 +0530

    feat(particles): expand max particle limit to 200

--- src/particles.js
+++ src/particles.js
@@ -5,7 +5,7 @@ export class ParticleSystem {
     constructor(scene) {
         this.scene = scene;
         this.particles = [];
-        this.maxParticles = 150;
+        this.maxParticles = 200;
     }
 
     emit(x, y, z, type) {
