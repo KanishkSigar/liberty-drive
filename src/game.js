1decf206e44c2b56c8f9a311a5b91cef9bab2040
KanishkSigar <kanishksigar@gmail.com>
2026-08-08 12:30:02 +0530

    tune(wanted): boost collision heat penalty to 15 points

--- src/game.js
+++ src/game.js
@@ -385,7 +385,7 @@ class Game {
         this.audio.updateEngine(this.car.mph, this.input.is('accelerate'));
 
         if (wasCollided) {
-            this.wanted.addHeat(12);
+            this.wanted.addHeat(15);
             this._screenShake = 0.4;
             this.audio.playCrash();
             // Spawn sparks at car front
