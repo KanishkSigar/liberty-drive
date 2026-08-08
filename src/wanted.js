7af75de40370cd8a4cc2dc9d624d7e5f0996654a
KanishkSigar <kanishksigar@gmail.com>
2026-08-08 08:09:34 +0530

    feat(wanted): add siren audio timer tracking to wanted manager

--- src/wanted.js
+++ src/wanted.js
@@ -8,6 +8,7 @@ export class WantedManager {
         this.stars = 0; // 0 to 3 stars
         this.heat = 0; // Heat points (0 to 100)
         this.policeCars = [];
+        this.sirenTimer = 0;
         this.elements = {
             container: document.getElementById('hud-wanted'),
             stars: document.querySelectorAll('.wanted-star')
