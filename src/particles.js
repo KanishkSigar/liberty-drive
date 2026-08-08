699e5d681f1e214934ff0766f4157eea65eec5f0
KanishkSigar <kanishksigar@gmail.com>
2026-08-08 13:36:00 +0530

    feat(particles): increase max particle buffer capacity to 220

--- src/particles.js
+++ src/particles.js
@@ -5,7 +5,7 @@ export class ParticleSystem {
     constructor(scene) {
         this.scene = scene;
         this.particles = [];
-        this.maxParticles = 200;
+        this.maxParticles = 220;
     }
 
     emit(x, y, z, type) {
