59b71f214f8bf714bd1e1c566a49addc4be23883
KanishkSigar <kanishksigar@gmail.com>
2026-08-09 12:27:49 +0530

    feat(wanted): implement setAudio method for siren sound activation

--- src/wanted.js
+++ src/wanted.js
@@ -13,7 +13,11 @@ export class WantedManager {
     getPolicePositions() {
         return this.policeCars.map(p => ({ x: p.mesh.position.x, z: p.mesh.position.z }));
         this.sirenTimer = 0;
-        this.audio = null;
+        this.audio = audio;
+    }
+
+    setAudio(audio) {
+        this.audio = audio;
         this.elements = {
             container: document.getElementById('hud-wanted'),
             stars: document.querySelectorAll('.wanted-star')
