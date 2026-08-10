281e7674108bc11811cdc5a7252953f14006aa23
KanishkSigar <kanishksigar@gmail.com>
2026-08-10 11:13:48 +0530

    tune(skid): expand skid marks buffer capacity to 80 decals

--- src/skid.js
+++ src/skid.js
@@ -5,7 +5,7 @@ export class SkidMarkManager {
     constructor(scene) {
         this.scene = scene;
         this.skids = [];
-        this.maxSkids = 60;
+        this.maxSkids = 80;
     }
 
     addSkid(x, z, angle, width = 0.5) {
