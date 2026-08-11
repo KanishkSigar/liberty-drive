14609b60a39b803afea09470a5e7ed92873c3cff
KanishkSigar <kanishksigar@gmail.com>
2026-08-11 16:01:10 +0530

    tune(skid): expand skid marks decal capacity to 100 ribbons

--- src/skid.js
+++ src/skid.js
@@ -5,7 +5,7 @@ export class SkidMarkManager {
     constructor(scene) {
         this.scene = scene;
         this.skids = [];
-        this.maxSkids = 90;
+        this.maxSkids = 100;
     }
 
     addSkid(x, z, angle, width = 0.55) {
