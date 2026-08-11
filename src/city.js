140e9bbad9a6c2f99bc2443a654f2c3727cd43c1
KanishkSigar <kanishksigar@gmail.com>
2026-08-11 15:20:32 +0530

    tune(city): extend street light illumination radius to 32 units

--- src/city.js
+++ src/city.js
@@ -667,7 +667,7 @@ export class City {
                     this.scene.add(pole);
 
         // Street light head emissive cone
-        const lampGlow = new THREE.PointLight(0xffdd99, 1.2, 28);
+        const lampGlow = new THREE.PointLight(0xffdd99, 1.4, 32);
         lampGlow.position.set(cx, 16, cz);
         this.scene.add(lampGlow);
 
