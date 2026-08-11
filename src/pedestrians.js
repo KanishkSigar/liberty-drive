7bf620f5449be1a3da5a23bb68f814b77ef7d4ce
KanishkSigar <kanishksigar@gmail.com>
2026-08-11 09:50:58 +0530

    tune(pedestrians): boost pedestrian panic evasion sprint multiplier

--- src/pedestrians.js
+++ src/pedestrians.js
@@ -104,7 +104,7 @@ export class PedestrianManager {
             if (dist < 18) {
                 p.isPanicking = true;
                 // Move away from car
-                p.x -= (dx / (dist + 0.1)) * (p.speed * 2.5) * dt;
+                p.x -= (dx / (dist + 0.1)) * (p.speed * 2.8) * dt;
                 p.z -= (dz / (dist + 0.1)) * (p.speed * 2.5) * dt;
             } else {
                 p.isPanicking = false;
