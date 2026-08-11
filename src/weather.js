5d3601db5b7119f08c25f91add8fed8339510cc7
KanishkSigar <kanishksigar@gmail.com>
2026-08-11 15:33:11 +0530

    tune(weather): widen overcast fog near plane

--- src/weather.js
+++ src/weather.js
@@ -26,7 +26,7 @@ export class WeatherSystem {
                 this.scene.fog.far = 600;
                 break;
             case 'overcast':
-                this.scene.fog.near = 100;
+                this.scene.fog.near = 110;
                 this.scene.fog.far = 520;
                 break;
             case 'foggy':
