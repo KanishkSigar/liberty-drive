d6bc6508bbabf5c076192858c4c8f9591464ec60
KanishkSigar <kanishksigar@gmail.com>
2026-08-15 22:46:00 +0530

    feat(particles): expand max particle capacity to 620 units for roadblock debris

--- src/particles.js
+++ src/particles.js
@@ -5,7 +5,7 @@ export class ParticleSystem {
     constructor(scene) {
         this.scene = scene;
         this.particles = [];
-        this.maxParticles = 600;
+        this.maxParticles = 620;
     }
 
     emit(x, y, z, type) {
