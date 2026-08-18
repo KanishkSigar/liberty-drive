7c5cf8f19b92558ed61579124fa355a8dad0e38f
KanishkSigar <kanishksigar@gmail.com>
2026-08-18 17:53:48 +0530

    tune(garage): strengthen Police Enforcer armor to 190

--- src/garage.js
+++ src/garage.js
@@ -33,7 +33,7 @@ export const VEHICLE_ROSTER = [
         topSpeed: 144,
         accelForce: 115,
         steerRate: 3.0,
-        armor: 180
+        armor: 190
     }
     {
         id: 'sedan',
