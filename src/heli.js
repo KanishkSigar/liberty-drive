86c6c9acee3c9a75566769bb07feca3b6d482d03
KanishkSigar <kanishksigar@gmail.com>
2026-08-18 10:58:17 +0530

    tune(heli): raise helicopter cruise altitude to 52 units

--- src/heli.js
+++ src/heli.js
@@ -11,7 +11,7 @@ export class HelicopterManager {
         this.rotorSpeed = 0;
         this.x = 0;
         this.z = 0;
-        this.altitude = 48;
+        this.altitude = 52;
     }
 
     spawn(playerX, playerZ) {
