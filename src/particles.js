9a8abc57db6464e043cf22fa9b84c47cb02f4680
KanishkSigar <kanishksigar@gmail.com>
2026-08-19 10:48:00 +0530

    feat(particles): expand max particle capacity to 750 units for nitrous purge

--- src/particles.js
+++ src/particles.js
@@ -5,7 +5,7 @@ export class ParticleSystem {
     constructor(scene) {
         this.scene = scene;
         this.particles = [];
-        this.maxParticles = 720;
+        this.maxParticles = 750;
     }
 
     emit(x, y, z, type) {
