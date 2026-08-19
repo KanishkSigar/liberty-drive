51e981e2a753bd187ffdc76b9f9edf1c48346b82
KanishkSigar <kanishksigar@gmail.com>
2026-08-19 09:51:29 +0530

    tune(heli): optimize police helicopter cruise altitude to 56 units

--- src/heli.js
+++ src/heli.js
@@ -11,7 +11,7 @@ export class HelicopterManager {
         this.rotorSpeed = 0;
         this.x = 0;
         this.z = 0;
-        this.altitude = 55;
+        this.altitude = 56;
     }
 
     spawn(playerX, playerZ) {
