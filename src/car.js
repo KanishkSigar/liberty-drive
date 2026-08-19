7bb427749ba44e8717a23a3fbc7f48a2a9adb293
KanishkSigar <kanishksigar@gmail.com>
2026-08-19 09:15:23 +0530

    tune(car): increase nitro fuel recharge rate to 15 units/s

--- src/car.js
+++ src/car.js
@@ -32,7 +32,7 @@ export class Car {
         this.tiresPopped = false;
         this.nitro = 100;
         this.maxNitro = 100;
-        this.nitroRegenRate = 14;
+        this.nitroRegenRate = 15;
         this.isBoosting = false;
         this.hd = 5.2;
 
