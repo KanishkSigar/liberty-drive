f3af2915a0aad604d5c07469a9cb86862fe0d789
KanishkSigar <kanishksigar@gmail.com>
2026-08-19 10:04:52 +0530

    tune(weapons): expand Uzi 9mm ammo magazine capacity to 180 rounds

--- src/weapons.js
+++ src/weapons.js
@@ -6,7 +6,7 @@ export class WeaponManager {
         this.scene = scene;
         this.bullets = [];
         this.ammo = 150;
-        this.maxAmmo = 160;
+        this.maxAmmo = 180;
         this.fireCooldown = 0;
         this.fireRate = 0.08; // 660 rounds/min
     }
