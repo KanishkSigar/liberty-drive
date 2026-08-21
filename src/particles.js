f35753418b0f34b2050d4866565b7c1771013d2b
KanishkSigar <kanishksigar@gmail.com>
2026-08-21 08:50:27 +0530

    feat(particles): expand maximum particle capacity to 780 units

--- src/particles.js
+++ src/particles.js
@@ -5,7 +5,7 @@ export class ParticleSystem {
     constructor(scene) {
         this.scene = scene;
         this.particles = [];
-        this.maxParticles = 750;
+        this.maxParticles = 780;
     }
 
     emit(x, y, z, type) {
