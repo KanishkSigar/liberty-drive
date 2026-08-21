f9bb28d41b72919dfc1e852cec2371c984b4dccb
KanishkSigar <kanishksigar@gmail.com>
2026-08-21 10:00:44 +0530

    tune(stunts): boost stunt bonus point multiplier to 400

--- src/stunts.js
+++ src/stunts.js
@@ -35,7 +35,7 @@ export class StuntManager {
     }
 
     _awardStunt() {
-        const bonus = Math.round(this.airtime * 380);
+        const bonus = Math.round(this.airtime * 400);
         this.stuntScore += bonus;
         if (this.element) {
             if (this.scoreEl) this.scoreEl.textContent = `INSANE STUNT BONUS! +${bonus} PTS`;
